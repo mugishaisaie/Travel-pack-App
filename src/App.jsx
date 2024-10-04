809c5e4ec6576432e351e9cf686554e7cc99da54
mugisha isaie <mugishaisaieog1@gmail.com>
2024-10-04 15:15:39 +0200

    creating 3 methods to sort by and iterating over items with sortedItems and creating clearList button

--- src/App.jsx
+++ src/App.jsx
@@ -23,12 +23,19 @@ function App() {
   const handleToggleItem = (id)=>{
     setItems((items)=>items.map((item)=>item.id === id ? {...item, packed:!item.packed}: item))
   }
+
+  function handleClearList(){
+    setItems([])
+    
+  }
+
+
   return  (
     <div className='app'>
     <Logo />
   <Form handleAddItem={handleAddItem} />
     <PackingList items={items} handleDeleteItem={handleDeleteItem} 
-    handleToggleItem={handleToggleItem}/>
+    handleToggleItem={handleToggleItem} handleClearList={handleClearList}/>
     <Stats items={items} />
     </div>
   )
@@ -78,13 +85,33 @@ const Form = ({handleAddItem})=>{
     </form>
   )
 }
-const PackingList = ({items, handleDeleteItem,handleToggleItem})=>{
+const PackingList = ({items, handleDeleteItem,handleToggleItem,handleClearList})=>{
+
+  const [sortBy, setSortBy] = useState('input');
+
+  let sortedItems;
+  if(sortBy === 'input') sortedItems = items;
+  if(sortBy === 'description') sortedItems = items.slice().sort((a,b)=>(a.description.localeCompare(b.description)));
+  if(sortBy === 'packed') sortedItems = items.slice().sort((a,b)=>Number(b.packed - a.packed));
+
+ 
   
   
   return <div className="list">
     <ul>
-    {items.map((item)=> <Item key={item.id} item={item} handleDeleteItem={handleDeleteItem} handleToggleItem={handleToggleItem}/>)}
+    {sortedItems.map((item)=> <Item key={item.id} item={item} handleDeleteItem={handleDeleteItem} handleToggleItem={handleToggleItem}/>)}
   </ul>
+
+  <div className='actions'>
+    <select value={sortBy} onChange={e=>setSortBy(e.target.value)}>
+      <option value="input">Sort By Input</option>
+      <option value="description">Sort By Description</option>
+      <option value="packed">Sort By Packed Status</option>
+    </select>
+
+    <button className='btn' onClick={handleClearList}>Clear List</button>
+
+  </div>
   </div>
 }
 
